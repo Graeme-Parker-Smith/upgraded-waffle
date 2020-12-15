33e6d4082bd818e686975efd4ef5ef7c730efe19
Graeme <graemster@gmail.com>
2020-12-14 18:29:26 -0600

    edit transporter log

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const path = require('path');
 const generatePassword = require('password-generator');
 
-
 const app = express();
 
 // Serve static files from the React app
@@ -32,9 +31,9 @@ const transporter =
 			pass: process.env.MAILPASS,
 		},
 	});
-console.log('transporter is: ', transporter);
-
-// add middleware
+if (transporter) {
+	console.log('transporter exists.');
+}
 
 app.get('/api/test', (req, res) => {
 	console.log('test route activated');
@@ -68,26 +67,22 @@ app.post('/api/sendemail', (req, res) => {
 	}
 });
 
-
-
 app.get('/api/passwords', (req, res) => {
-  const count = 5;
+	const count = 5;
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
+	// Generate some passwords
+	const passwords = Array.from(Array(count).keys()).map((i) => generatePassword(12, false));
 
-  // Return them as json
-  res.json(passwords);
+	// Return them as json
+	res.json(passwords);
 
-  console.log(`Sent ${count} passwords`);
+	console.log(`Sent ${count} passwords`);
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
